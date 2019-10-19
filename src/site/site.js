1de785d6dc4e9636b1be60965dff3b7939098244
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-19 14:05:11 -0600

    Wrap site component in link to dynamic link with :siteId

--- src/site/site.js
+++ src/site/site.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./site.css";
+import { Link } from "react-router-dom";
 
 export default function Site(props) {
   return (
-    <div className="site">
-      <h2>{props.title}</h2>
-      <h3>{props.state}</h3>
-      <img src={props.imgSrc} alt="Trash site" />
-    </div>
+    <Link to={`/sites/${props.id}`}>
+      <div className="site">
+        <h2>{props.title}</h2>
+        <h3>{props.state}</h3>
+        <img src={props.imgSrc} alt="Trash site" />
+      </div>
+    </Link>
   );
 }
