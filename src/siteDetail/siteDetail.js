90edd601e928b4c49e1b0cda04c3f5c276907b20
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-19 17:28:57 -0600

    update siteDetail to filter passed siteList via context and return Site component

--- src/siteDetail/siteDetail.js
+++ src/siteDetail/siteDetail.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import siteContext from "../siteContext";
+import Site from "../site/site";
 
 export default class SiteDetail extends Component {
+  static contextType = siteContext;
+
   render() {
+    const selectedSite = this.context.sites.sites.find(site => {
+      const numberProp = parseInt(this.props.match.params.siteId);
+      if (site.id === numberProp) {
+        return site;
+      }
+    });
     return (
       <div className="siteDetail">
-        <h2>HEY</h2>
+        <Site
+          key={selectedSite.id}
+          id={selectedSite.id}
+          title={selectedSite.title}
+          address={selectedSite.address}
+          state={selectedSite.state}
+          description={selectedSite.description}
+          imgSrc={selectedSite.imgSrc}
+        />
       </div>
     );
   }
