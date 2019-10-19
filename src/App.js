4d18ab6fef85a473f0ab7a954872d6955ec809c4
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-19 17:21:26 -0600

    Wrap Routes in siteContext.Provider with contenxtValue of state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Route } from "react-router-dom";
+
+import "./App.css";
+
+// Custom Components
 import Nav from "./nav/nav";
 import Landing from "./landing/landing";
 import About from "./about/about";
@@ -8,16 +11,33 @@ import SignIn from "./signIn/signIn";
 import SiteList from "./siteList/siteList";
 import SiteDetail from "./siteDetail/siteDetail";
 
+// Context
+import siteContext from "./siteContext";
+
+// Seed data
+import Sites from "./siteData";
+
 class App extends Component {
+  state = {
+    // Initially set to seed data
+    sites: Sites
+  };
+
   render() {
+    const contextValue = {
+      sites: this.state.sites
+    };
     return (
       <div className="App">
-        <Nav />
-        <Route path="/about" component={About} />
-        <Route path="/signIn" component={SignIn} />
-        <Route path="/sites" component={SiteList} />
-        <Route path="sites/:siteId" component={SiteDetail} />
-        <Route exact path="/" component={Landing} />
+        <siteContext.Provider value={contextValue}>
+          <Nav />
+          <Route path="/about" component={About} />
+          <Route path="/signIn" component={SignIn} />
+          <Route path="/sites" component={SiteList} />
+          <Route path="sites/:siteId" component={SiteDetail} />
+          <Route exact path="/" component={Landing} />
+        </siteContext.Provider>
+        }
       </div>
     );
   }
