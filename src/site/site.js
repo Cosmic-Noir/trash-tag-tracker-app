2d3e247d232f797ce76bd776819d0b321866cca7
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-19 13:38:40 -0600

    Sucess passing img src to List

--- src/site/site.js
+++ src/site/site.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./site.css";
 
 export default function Site(props) {
   return (
     <div className="site">
       <h2>{props.title}</h2>
+      <h3>{props.state}</h3>
+      <img src={props.imgSrc} />
     </div>
   );
 }
