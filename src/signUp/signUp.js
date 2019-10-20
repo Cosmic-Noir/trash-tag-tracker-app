02b9fccebae316aec1e830e5f9abdb9e40aefbdf
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-20 15:35:19 -0600

    Add signUp validation

--- src/signUp/signUp.js
+++ src/signUp/signUp.js
@@ -33,25 +33,76 @@ class SignUp extends Component {
   handlSubmit = e => {
     e.preventDefault();
     console.log(`Create Account pressed, checking info...`);
+    let matchingUser = this.context.users.find(user => {
+      if (
+        user.email === this.state.email ||
+        user.username === this.state.username
+      ) {
+        return "Error, matching username or email already registered";
+      }
+    });
 
     // Check if info empty or missing:
     if (this.state.username.length < 6) {
-      console.log("does");
+      this.setState({
+        error: `Username length must be greater than 5 characters`
+      });
+    } else if (this.state.password.length < 6) {
+      this.setState({
+        error: `Password length must be greater than 5 characters`
+      });
+    } else if (matchingUser !== undefined) {
+      this.setState({
+        error: `Username or e-mail already registered`
+      });
+    } else {
+      this.updateId();
+      let newUser = this.state;
+      console.log(
+        `New user successfully passed inspection with info: ${newUser}`
+      );
     }
-
-    // Check if username or e-mail taken, update error for problems
   };
+
+  // Check if username or e-mail taken, update error for problems
 
   render() {
     return (
       <div className="signUp">
-        <form>
+        <form
+          onSubmit={e => {
+            this.handlSubmit(e);
+          }}
+        >
           <label>Username:</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            ref={this.username}
+            onChange={e => this.updateUsername(e.target.value)}
+          ></input>
           <label>Email:</label>
-          <input type="email"></input>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            ref={this.email}
+            onChange={e => this.updateEmail(e.target.value)}
+          ></input>
           <label>Password:</label>
-          <input type="password"></input>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            ref={this.password}
+            onChange={e => this.updatePass(e.target.value)}
+          />
+          {this.state.error !== null ? (
+            <h4 className="error">{this.state.error}</h4>
+          ) : (
+            ""
+          )}
           <button type="submit">Create Account</button>
         </form>
         <Link to="/signIn">Sign In</Link>
