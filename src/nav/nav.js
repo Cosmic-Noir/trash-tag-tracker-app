4a56f2bec6422ffadcd9612cbf19a1da6136b31f
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-20 13:28:29 -0600

    Update nav to conditionally render showing Sign In component

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import siteContext from "../siteContext";
 import "./nav.css";
 
-export default function Nav() {
-  return (
-    <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/sites">Trash Sites</Link>
-      <Link to="/signIn">Sign In</Link>
-    </div>
-  );
+class Nav extends Component {
+  static contextType = siteContext;
+
+  render() {
+    return (
+      <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/sites">Trash Sites</Link>
+        {this.context.loggedIn === false ? (
+          <Link to="/signIn">Sign In</Link>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
+
+export default Nav;
