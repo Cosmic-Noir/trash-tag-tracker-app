9cd9ece98092fdda697eb7c7ad4ea5d0c57fb428
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-20 13:51:19 -0600

    Update siteContext for passed methods

--- src/siteContext.js
+++ src/siteContext.js
@@ -3,7 +3,9 @@ import React from "react";
 const siteContext = React.createContext({
   sites: [],
   loggedIn: "",
-  userInfo: []
+  userInfo: [],
+  onLogIn: () => {},
+  setUserInfo: () => {}
 });
 
 export default siteContext;
