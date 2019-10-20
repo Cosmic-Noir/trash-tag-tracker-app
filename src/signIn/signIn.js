0a5fe77f6e26963cd5d64289bc0d494c24af1585
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-20 13:32:32 -0600

    Update signIn to class to use context to update user database

--- src/signIn/signIn.js
+++ src/signIn/signIn.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import siteContext from "../siteContext";
 
-export default function SignIn() {
-  return (
-    <div className="signIn">
-      <h2>Sign In: </h2>
-      <form>
-        <label>E-mail:</label>
-        <input type="email" />
-        <label>Password:</label>
-        <input type="password" />
-        <button type="submit">Log In</button>
-      </form>
-    </div>
-  );
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="signIn">
+        <h2>Sign In: </h2>
+        <form>
+          <label>E-mail:</label>
+          <input type="email" />
+          <label>Password:</label>
+          <input type="password" />
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default SignIn;
