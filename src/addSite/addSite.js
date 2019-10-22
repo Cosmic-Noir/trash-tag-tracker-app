bc5160012bfff33407bbb7afb258a66562f0616c
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-21 18:18:00 -0600

    Add state changing functions and handleForm function - works

--- src/addSite/addSite.js
+++ src/addSite/addSite.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react";
+import siteContext from "../siteContext";
 
 class AddSite extends Component {
-  state = {};
+  state = {
+    id: "",
+    clean: "false",
+    title: "",
+    address: "",
+    state: "",
+    description: "",
+    beforeImg: "",
+    afterImg: ""
+  };
+
+  static contextType = siteContext;
+
+  // Methods to update state
+  updateId() {
+    this.setState({ id: Math.floor(Math.random() * 1000), error: null });
+  }
+
+  updateTitle(title) {
+    this.setState({ title: title });
+  }
+
+  updateAddress(address) {
+    this.setState({ address: address });
+  }
+
+  updateState(state) {
+    this.setState({ state: state });
+  }
+
+  updateDescription(description) {
+    this.setState({ descrtiption: description });
+  }
+
+  updateBeforeImg(beforeImg) {
+    this.setState({ beforeImg: beforeImg });
+  }
+
+  // submit info in form
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submit form pressed");
+  };
 
   render() {
     return (
       <div className="addSite">
         <h2>Add a new site that needs cleaning:</h2>
-        <form>
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+        >
           <label htmlFor="title">Title</label>
           <input type="text"></input>
           <label htmlFor="address">Address(aprox):</label>
@@ -22,6 +69,7 @@ class AddSite extends Component {
           <textarea></textarea>
           <label htmlFor="beforeImg">Photo web address:</label>
           <input type="text"></input>
+          <button type="submit">Add Site</button>
         </form>
       </div>
     );
