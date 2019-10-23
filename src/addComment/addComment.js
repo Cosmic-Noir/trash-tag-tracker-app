82b4f63afe30b37093b331feb4d789a76811957c
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-23 17:19:35 -0600

    Add submit button and handleSubmit function with state update functions

--- src/addComment/addComment.js
+++ src/addComment/addComment.js
@@ -3,9 +3,36 @@ import siteContext from "../siteContext";
 import "./addComment.css";
 
 class AddComment extends Component {
-  state = {};
+  state = {
+    id: "",
+    siteId: "",
+    userRef: "",
+    content: ""
+  };
 
   static contextType = siteContext;
+
+  // Methods to update state
+  updateId = () => {
+    let newId = Math.floor(Math.random() * 1000);
+    this.setState({ id: newId });
+  };
+
+  updateUserRef = userRef => {
+    this.setState({ userRef: userRef });
+  };
+
+  updateContent = content => {
+    this.setState({ content: content });
+  };
+
+  // Method to send newComment to App state.comments
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("form pressed");
+  };
+
+  // Methods for buttons
 
   handleCancel = () => {
     const element = document.getElementById("addComment");
@@ -17,7 +44,15 @@ class AddComment extends Component {
   render() {
     return (
       <div className="addComment hidden" id="addComment">
-        <h3>Add comment:</h3>
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+        >
+          <label htmlFor="content">Add comment:</label>
+          <textarea></textarea>
+          <button type="submit">Add</button>
+        </form>
 
         <button type="button" id="cancel" onClick={this.handleCancel}>
           Cancel
