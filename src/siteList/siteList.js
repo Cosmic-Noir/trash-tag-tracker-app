a1ba9de25b8898ded65c597fc6ce3ee06f2647f5
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-01 16:07:05 -0600

    SUCCESS requesting trash sites and creating outside of render

--- src/siteList/siteList.js
+++ src/siteList/siteList.js
@@ -7,7 +7,9 @@ import "./siteList.css";
 
 class SiteList extends Component {
   state = {
-    state_abr: ""
+    error: "",
+    state_abr: "",
+    sites: null
   };
 
   static contextType = siteContext;
@@ -16,10 +18,33 @@ class SiteList extends Component {
     this.setState({ state_abr: state_abr });
   };
 
-  componentDidMount() {}
+  // Get trashSites
+  getTrashSites = () => {
+    const url = config.API_ENDPOINT + "sites/trash";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "content-type": "applicatin/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then(this.setSites)
+      .then(console.log("Request for all sites sent"))
+      .catch(error => this.setState({ error }));
+  };
 
-  render() {
-    const sites = this.context.sites.map(site => {
+  // Set state of trashSites
+  setSites = sites => {
+    this.setState({ sites: sites, error: null });
+  };
+
+  displayList = () => {
+    const sites = this.state.sites.map(site => {
       return (
         <Site
           key={site.id}
@@ -31,7 +56,21 @@ class SiteList extends Component {
         />
       );
     });
-    console.log(this.context.sites);
+    return sites;
+  };
+
+  filterForStates = trashSites => {
+    const stateOptions = trashSites.map(site => {
+      return site.state_abr;
+    });
+  };
+
+  componentDidMount() {
+    this.getTrashSites();
+  }
+
+  render() {
+    // console.log(this.context.sites);
     // // Create array of all states there are sites for
     // const stateOptions = this.context.sites.map(site => {
     //   if (site.clean === "false") {
@@ -71,7 +110,11 @@ class SiteList extends Component {
           </select>
         </form>
         <ul className="siteList" id="siteList">
-          {sites}
+          {this.state.error === null ? (
+            this.displayList()
+          ) : (
+            <p>{this.state.error}</p>
+          )}
         </ul>
         <footer>
           {this.context.loggedIn === false ? (
