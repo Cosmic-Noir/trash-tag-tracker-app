b10cdbb6d5e67d4372d96fd0e5b642a21317f668
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-05 17:00:19 -0700

    Remove Mission link from dashboard

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -12,9 +12,10 @@ class Dashboard extends Component {
         {/* <h3>EnvoScore: </h3> */}
         <h3>What would you like to do?</h3>
         <Link to="/sites">View Trash Sites</Link>
+        <Link to="/cleaned">View Cleaned Sites</Link>
         <Link to="/addSite">Add New Trash Site</Link>
-        <Link to="/about">View Our Mission</Link>
-        <Link to="/donate">Donate</Link>
+        {/* <Link to="/about">View Our Mission</Link>
+        <Link to="/donate">Donate</Link> */}
       </div>
     );
   }
