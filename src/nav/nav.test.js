3a1dba50a56ec8943d2fbc888d83ae0240139dc5
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-09 11:37:07 -0700

    Nav tests pass

---
+++ src/nav/nav.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDom from "react-dom";
+import renderer from "react-test-renderer";
+import Nav from "./nav";
+import { BrowserRouter } from "react-router-dom";
+
+describe("CommentList Component", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>,
+      div
+    );
+    ReactDom.unmountComponentAtNode(div);
+  });
+  it("Renders the UI as expected", () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    ).toJSON;
+    expect(tree).toMatchSnapshot();
+  });
+});
