fe10ec3f80ff5648d489d540e08de27731ff1ea3
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-09 11:40:12 -0700

    signOut test pass

---
+++ src/signOut/signOut.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import renderer from "react-test-renderer";
+import SignOut from "./signOut";
+
+describe("SignOut component", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(<SignOut />, div);
+    ReactDom.unmountComponentAtNode(div);
+  });
+  it("Renders the UI as expected", () => {
+    const tree = renderer.create(<SignOut />).toJSON;
+    expect(tree).toMatchSnapshot();
+  });
+});
