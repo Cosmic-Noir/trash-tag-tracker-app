f38fafdcbe9b116b05135625de30a9adc1f77c3b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-09 11:41:58 -0700

    site tests pass

---
+++ src/site/site.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDom from "react-dom";
+import renderer from "react-test-renderer";
+import Site from "./site";
+import { BrowserRouter } from "react-router-dom";
+
+describe("SignUp Component", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(
+      <BrowserRouter>
+        <Site />
+      </BrowserRouter>,
+      div
+    );
+    ReactDom.unmountComponentAtNode(div);
+  });
+  it("Renders the UI as expected", () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Site />
+      </BrowserRouter>
+    ).toJSON;
+    expect(tree).toMatchSnapshot();
+  });
+});
