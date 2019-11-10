40955a291bcf35c0bc80b44e903afa5b06528099
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-10 14:49:40 -0700

    Update cleanList to ES5 syntax

--- src/cleanList/cleanList.js
+++ src/cleanList/cleanList.js
@@ -19,19 +19,20 @@ class CleanList extends Component {
   displayList = () => {
     // eslint-disable-next-line
     const sites = this.context.clean_sites.map(site => {
+      const { id, title, before_img, after_img, city, state_abr } = site;
       if (
         this.state.state_abr === "" ||
         this.state.state_abr === site.state_abr
       ) {
         return (
           <Site
-            key={site.id}
-            id={site.id}
-            title={site.title}
-            before_img={site.before_img}
-            after_img={site.after_img}
-            city={site.city}
-            state_abr={site.state_abr}
+            key={id}
+            id={id}
+            title={title}
+            before_img={before_img}
+            after_img={after_img}
+            city={city}
+            state_abr={state_abr}
           />
         );
       }
@@ -44,15 +45,11 @@ class CleanList extends Component {
 
     return this.context.clean_sites.map(site => {
       const { state_abr } = site;
-      console.log(state_abrs);
-      console.log("state is " + state_abr);
       // Adding state_abr to state_abrs object
       // if state_abr already exists - then return null
       if (state_abrs[state_abr] === true) {
-        console.log("State is already on the list, not adding to object");
         return null;
       } else {
-        console.log("State not on list, adding to list and returning option");
         // else if state_abr does not exist - then return option
         state_abrs[state_abr] = true;
 
