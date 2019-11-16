52e88e2ff7204348e60e552ea19ed791d716a3b6
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-16 11:41:04 -0700

    Change API_ENDPOINT back to hosted server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com/api/`,
-  API_ENDPOINT: `http://localhost:8000/api/`,
+  API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com/api/`,
+  // API_ENDPOINT: `http://localhost:8000/api/`,
   TOKEN_KEY: "Trash-Tracker-Auth-Token",
   API_KEY: process.env.REACT_APP_API_KEY
 };
