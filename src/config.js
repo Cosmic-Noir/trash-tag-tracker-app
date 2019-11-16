33491b52751a5dc0673a180e59982b15babf3e3d
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-16 09:08:51 -0700

    Update API_endpoint with missing slash

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com`,
+  API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com/`,
   TOKEN_KEY: "Trash-Tracker-Auth-Token"
 };
