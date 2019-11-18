5692cb3031d57112d8409c5b048e4c87c02e33b0
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-18 16:48:23 -0700

    Add mobile menu icon for smaller screen widths, hide nav links

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -5,6 +5,7 @@ import TokenService from "../auth/token-service";
 
 // Styling and Images:
 import logo from "./logo1.png";
+import menuIcon from "./menuIcon.png";
 import "./nav.css";
 
 class Nav extends Component {
@@ -21,33 +22,38 @@ class Nav extends Component {
         <Link to="/" className="nav" id="navLogo">
           <img src={logo} alt="Logo" className="logo" />
         </Link>
-        TrashTracker
-        <Link to="/" className="nav">
+
+        <Link to="/" className="nav deskNav">
           Home
         </Link>
         {/* <Link to="/about">Impact</Link> */}
-        <Link to="/sites" className="nav">
+        <Link to="/sites" className="nav deskNav">
           Trash Sites
         </Link>
-        <Link to="/cleaned" className="nav">
+        <Link to="/cleaned" className="nav deskNav">
           Cleaned Sites
         </Link>
         {this.context.loggedIn === true ? (
-          <Link to="/dashboard" className="nav">
+          <Link to="/dashboard" className="nav deskNav">
             Dashboard
           </Link>
         ) : (
           ""
         )}
         {this.context.loggedIn === false ? (
-          <Link to="/signUp" className="nav" id="signUp">
+          <Link to="/signUp" className="nav deskNav" id="signUp">
             Sign Up
           </Link>
         ) : (
-          <Link to="/signOut" className="nav" onClick={this.handleLogOut}>
+          <Link
+            to="/signOut"
+            className="nav deskNav"
+            onClick={this.handleLogOut}
+          >
             Sign Out
           </Link>
         )}
+        <img src={menuIcon} alt="menu icon" id="menuIcon" />
       </div>
     );
   }
