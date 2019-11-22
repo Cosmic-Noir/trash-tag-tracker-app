987246449f895bd41f63e329a77bd0f1450a5b14
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-21 17:58:26 -0700

    Update cleanSite comp style

--- src/cleanSite/cleanSite.js
+++ src/cleanSite/cleanSite.js
@@ -66,19 +66,22 @@ class CleanSite extends Component {
 
   render() {
     return (
-      <div className="clean flex-column">
-        <h2>Mark A Trash Site As Cleaned:</h2>
+      <div className="flex-column">
+        <h2 className="title">Mark A Trash Site As Cleaned:</h2>
         <h3>{this.state.title}</h3>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="description">Updated Description:</label>
+          <label className="subtitle" htmlFor="description">
+            Updated Description:
+          </label>
           <textarea
-            name="content"
             id="content"
-            // value={this.state.description}
-            required
+            name="content"
             onChange={e => this.updateContent(e.target.value)}
+            required
           />
-          <label htmlFor="after_img">Upload Image:</label>
+          <label className="subtitle" htmlFor="after_img">
+            Upload Image:
+          </label>
           <input
             id="after_img"
             onChange={e => this.updateAfterImg(e)}
