044de733b7c298e9d49f27d4c3e73d05053310d3
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-21 20:20:03 -0700

    Update signOut

--- src/signOut/signOut.js
+++ src/signOut/signOut.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class SignOut extends Component {
   render() {
     return (
-      <div className="signOut">
-        <h2>You have signed out!</h2>
+      <div className="signOut flex-column">
+        <h2 className="title">You have signed out!</h2>
       </div>
     );
   }
