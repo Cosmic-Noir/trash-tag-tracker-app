ae0b25da303050db150e53a8f451c29039dd7d8c
CosmicNoir <cosmicnoir90@gmail.com>
2019-11-23 10:39:47 -0700

    Update about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import siteContext from "../../siteContext";
+
+// Styling & Images
 import "./about.css";
 
 // Maybe use this as a score page or to track impact from score
+// Component not currently in use
 
 class About extends Component {
   static contextType = siteContext;
@@ -12,7 +15,6 @@ class About extends Component {
       <div className="about">
         <h2>Total EnvoScore of All Users:</h2>
         <h2>{this.context.totalScore}</h2>
-
         <p>
           The above score is displays all points earned by every user of the
           app. This part of the app is currently undergoing construction...{" "}
