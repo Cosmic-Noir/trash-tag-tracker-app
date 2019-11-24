0ab8004377227c5ecfc8d62bbb6d8cbddc3c289e
CosmicNoir <cosmicnoir90@gmail.com>
2019-11-24 11:33:03 -0700

    Update signOut.js - need to update to function

--- src/components/signOut/signOut.js
+++ src/components/signOut/signOut.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
+// Should turn into function when can test 
 class SignOut extends Component {
   render() {
     return (
-      <div className="signOut flex-column">
+      <div className="flex-column signOut">
         <h2 className="title">You have signed out!</h2>
       </div>
     );
