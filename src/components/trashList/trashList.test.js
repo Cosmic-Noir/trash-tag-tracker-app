4f62aa28dd864ac3e055858eadbc9b2e493baa55
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-26 09:47:27 -0700

    Update test for trashList - all tests pass

--- src/components/trashList/trashList.test.js
+++ src/components/trashList/trashList.test.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom";
 import renderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
 import TrashList from "./trashList";
 
 describe("TrashList component", () => {
   it("Renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDom.render(<TrashList />, div);
+    ReactDom.render(
+      <BrowserRouter>
+        <TrashList />
+      </BrowserRouter>,
+      div
+    );
     ReactDom.unmountComponentAtNode(div);
   });
   it("Renders the UI as expected", () => {
-    const tree = renderer.create(<TrashList />).toJSON;
+    const tree = renderer.create(
+      <BrowserRouter>
+        <TrashList />
+      </BrowserRouter>
+    ).toJSON;
     expect(tree).toMatchSnapshot();
   });
 });
