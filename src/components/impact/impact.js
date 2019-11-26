0543c106dc85cffb8bb421fef25accbd78a7005c
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-26 14:14:07 -0700

    Update Impact to show total score and sites cleaned

--- src/components/impact/impact.js
+++ src/components/impact/impact.js
@@ -18,16 +18,36 @@ class Impact extends Component {
   static contextType = siteContext;
 
   /* State updating methods */
+  updateTotalSitesCleaned = () => {
+    const numbSites = this.context.clean_sites.length;
+    this.setState({ totalSitesCleaned: numbSites });
+    const totalScore = numbSites * 5;
+    this.updateTotalScore(totalScore);
+  };
+
+  updateTotalScore = totalScore => {
+    this.setState({ totalScore: totalScore });
+  };
+
+  componentDidMount() {
+    this.updateTotalSitesCleaned();
+  }
 
   render() {
     return (
       <div className="flex-column impact">
         <h2>Impact</h2>
+        <h1 id="totalScore">{this.state.totalScore}</h1>
         <p>
           The above score is displays all points earned by every user of the
-          app. This part of the app is currently undergoing construction...{" "}
+          app. Users have cleaned a total of{" "}
+          <span id="totalSites">
+            <strong>{this.state.totalSitesCleaned}</strong>
+          </span>{" "}
+          sites!
         </p>
-        <p></p>
+        <br />
+        <p>Together we can make an impact!</p>
       </div>
     );
   }
