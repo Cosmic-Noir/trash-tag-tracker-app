f443dce749fe6b1264015e22f2a554a3d4439b40
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-29 12:46:12 -0700

    Update signOut.js to function rather than component

--- src/components/signOut/signOut.js
+++ src/components/signOut/signOut.js
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Should turn into function when can test
-class SignOut extends Component {
-  render() {
-    return (
-      <div
-        className="flex-column signOut"
-        data-aos="fade-in"
-        data-aos-duration="1000"
-      >
-        <h2 className="title">You have signed out!</h2>
-      </div>
-    );
-  }
+export default function SignOut() {
+  return (
+    <div
+      className="flex-column signOut"
+      data-aos="fade-in"
+      data-aos-duration="1000"
+    >
+      <h2 className="title">You have signed out!</h2>
+    </div>
+  );
 }
-
-export default SignOut;
