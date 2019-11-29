4d76d5820ef6275e2bf39a27febb14924ae59d36
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-29 12:26:17 -0700

    Update Dashboard to function instead of component

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,37 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-// Should perhaps turn this into a function, OR update with user info to display
-class Dashboard extends Component {
-  render() {
-    return (
-      <div
-        className="dashboard flex-column"
-        data-aos="fade-in"
-        data-aos-duration="2000"
+export default function Dashboard() {
+  return (
+    <div
+      className="dashboard flex-column"
+      data-aos="fade-in"
+      data-aos-duration="2000"
+    >
+      <h2 className="title">Welcome back!</h2>
+      <h3 className="subtitle">What would you like to do?</h3>
+      <Link className="whiteButton" to="/sites">
+        View Trash Sites
+      </Link>
+      <Link className="whiteButton" to="/cleaned">
+        View Cleaned Sites
+      </Link>
+      <Link className="whiteButton" to="/addSite">
+        Add New Trash Site
+      </Link>
+      <a
+        className="whiteButton"
+        href="https://www.epa.gov/trash-free-waters/preventing-trash-source-0"
+        rel="noopener noreferrer"
+        target="_blank"
       >
-        <h2 className="title">Welcome back!</h2>
-        <h3 className="subtitle">What would you like to do?</h3>
-        <Link className="whiteButton" to="/sites">
-          View Trash Sites
-        </Link>
-        <Link className="whiteButton" to="/cleaned">
-          View Cleaned Sites
-        </Link>
-        <Link className="whiteButton" to="/addSite">
-          Add New Trash Site
-        </Link>
-        <a
-          className="whiteButton"
-          href="https://www.epa.gov/trash-free-waters/preventing-trash-source-0"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Ways to Reduce Trash
-        </a>
-      </div>
-    );
-  }
+        Ways to Reduce Trash
+      </a>
+    </div>
+  );
 }
-
-export default Dashboard;
