918ae8f1764bb951e0b893946af8bc4be26db1f9
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-30 15:57:32 -0700

    Update endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com/api/`,
-  API_ENDPOINT: `http://localhost:8000/api/`,
+  API_ENDPOINT: `https://fathomless-sierra-82385.herokuapp.com/api/`,
+  // API_ENDPOINT: `http://localhost:8000/api/`,
   TOKEN_KEY: "Trash-Tracker-Auth-Token"
 };
